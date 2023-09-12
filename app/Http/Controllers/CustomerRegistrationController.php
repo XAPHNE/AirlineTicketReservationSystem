b4d02ae76c2d690247146bcff7cd250f313bb061
XAPHNE <suvnkr.xaphene@gmail.com>
2023-09-12 21:00:55 +0530

    Update

--- app/Http/Controllers/CustomerRegistrationController.php
+++ app/Http/Controllers/CustomerRegistrationController.php
@@ -9,6 +9,17 @@ class CustomerRegistrationController extends Controller{
         return view('add-user');
     }
     public function registerUser(Request $request){
+        $request->validate([
+            'username' => 'required',
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'dob' => 'required|date',
+            'password' => 'required|confirmed',
+            'password_confirmation' => 'required',
+            'mobileNumber' => 'required|numeric'
+
+        ]);
         echo "<pre>";
         print_r($request->all());
     }
--- resources/views/add-user.blade.php
+++ resources/views/add-user.blade.php
@@ -13,46 +13,14 @@
                     <h4 class="card-title text-center">Customer Registration</h4>
                     <form action="{{url('/')}}/customer-registration" method="post">
                         @csrf
-                        <div class="mb-3">
-                            <label for="username" class="form-label">Username :</label>
-                            <input type="text" name="username" id="username" class="form-control" placeholder="Enter your username" aria-describedby="usernameHelp">
-                            <small id="usernameHelp" class="text-muted">Required</small>
-                        </div>
-                        <div class="mb-3">
-                            <label for="firstName" class="form-label">First Name :</label>
-                            <input type="text" name="firstName" id="firstName" class="form-control" placeholder="Enter your first name" aria-describedby="firstName">
-                            <small id="firstName" class="text-muted">Required</small>
-                        </div>
-                        <div class="mb-3">
-                            <label for="lastName" class="form-label">Last Name :</label>
-                            <input type="text" name="lastName" id="lastName" class="form-control" placeholder="Enter your last name" aria-describedby="lastnameHelp">
-                            <small id="lastnameHelp" class="text-muted">Required</small>
-                        </div>
-                        <div class="mb-3">
-                            <label for="email" class="form-label">Email :</label>
-                            <input type="email" name="email" id="email" class="form-control" placeholder="Enter your email" aria-describedby="emailHelp">
-                            <small id="emailHelp" class="text-muted">Required</small>
-                        </div>
-                        <div class="mb-3">
-                            <label for="dob" class="form-label">Date of birth :</label>
-                            <input type="date" name="dob" id="dob" class="form-control" placeholder="dd/mm/yyyy" aria-describedby="dobHelp">
-                            <small id="dobHelp" class="text-muted">Required</small>
-                        </div>
-                        <div class="mb-3">
-                            <label for="password" class="form-label">Password :</label>
-                            <input type="password" name="password" id="password" class="form-control" placeholder="Enter your password" aria-describedby="passwordHelp">
-                            <small id="passwordHelp" class="text-muted">Required</small>
-                        </div>
-                        <div class="mb-3">
-                            <label for="confirmPassword" class="form-label">Confirm Password :</label>
-                            <input type="password" name="confirmPassword" id="confirmPassword" class="form-control" placeholder="Reenter your password" aria-describedby="confirmPasswordHelp">
-                            <small id="confirmPasswordHelp" class="text-muted">Required</small>
-                        </div>
-                        <div class="mb-3">
-                            <label for="mobileNumber" class="form-label">Mobile Number :</label>
-                            <input type="number" name="mobileNumber" id="mobileNumber" class="form-control" placeholder="Enter you mobile number" aria-describedby="mobileNumberHelp">
-                            <small id="mobileNumberHelp" class="text-muted">Required</small>
-                        </div>
+                        <x-input-fields type="text" name="username" label="Username:" placeholder="Enter your username"/>
+                        <x-input-fields type="text" name="firstName" label="First Name:" placeholder="Enter your first name"/>
+                        <x-input-fields type="text" name="lastName" label="Last Name:" placeholder="Enter your last name"/>
+                        <x-input-fields type="email" name="email" label="Email:" placeholder="Enter your email"/>
+                        <x-input-fields type="date" name="dob" label="Date of Birth:" placeholder="dd/mm/yyyy"/>
+                        <x-input-fields type="password" name="password" label="Password:" placeholder="Enter your password"/>
+                        <x-input-fields type="password" name="password_confirmation" label="Confirm Password:" placeholder="Confirm your password"/>
+                        <x-input-fields type="number" name="mobileNumber" label="Mobile Number:" placeholder="Enter your 10 digit mobile number"/>
                         <button type="submit" class="btn btn-primary">Register</button>
                     </form>
                 </div>
