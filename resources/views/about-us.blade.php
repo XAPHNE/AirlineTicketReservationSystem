466532854afdcf85b2f6d96f9e8ecddf353f71cf
XAPHNE <suvnkr.xaphene@gmail.com>
2023-09-26 19:14:11 +0530

    Added Volt Laravel Dashboard Template

--- resources/views/about-us.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.main')
-@push('title')
-    <title>About Us | Airline Ticket Reservation System</title>
-@endpush()
-@section('main-section')
-<h1 class="text-center">About Us</h1>
-@endsection()
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,31 @@
 <?php
 
+use App\Http\Livewire\BootstrapTables;
+use App\Http\Livewire\Components\Buttons;
+use App\Http\Livewire\Components\Forms;
+use App\Http\Livewire\Components\Modals;
+use App\Http\Livewire\Components\Notifications;
+use App\Http\Livewire\Components\Typography;
+use App\Http\Livewire\Dashboard;
+use App\Http\Livewire\Err404;
+use App\Http\Livewire\Err500;
+use App\Http\Livewire\ResetPassword;
+use App\Http\Livewire\ForgotPassword;
+use App\Http\Livewire\Lock;
+use App\Http\Livewire\Auth\Login;
+use App\Http\Livewire\Profile;
+use App\Http\Livewire\Auth\Register;
+use App\Http\Livewire\ForgotPasswordExample;
+use App\Http\Livewire\Index;
+use App\Http\Livewire\LoginExample;
+use App\Http\Livewire\ProfileExample;
+use App\Http\Livewire\RegisterExample;
+use App\Http\Livewire\Transactions;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\CustomerRegistrationController;
-use App\Http\Controllers\EmployeeController;
-use App\Models\Customer;
+use App\Http\Livewire\ResetPasswordExample;
+use App\Http\Livewire\Roles;
+use App\Http\Livewire\UpgradeToPro;
+use App\Http\Livewire\Users;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,24 +33,41 @@ use App\Models\Customer;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/about-us', function () {
-        return view('about-us');
-});
-Route::get('/customer/register', [CustomerRegistrationController::class, 'addUserForm'])->name('customer.register');
-Route::post('/customer/register', [CustomerRegistrationController::class, 'registerUser']);
+Route::redirect('/', '/login');
 
-Route::get('/customer', [CustomerController::class, 'index']);
-Route::post('/customer', [CustomerController::class, 'store']);
-Route::get('/customer/view', [CustomerController::class, 'view']);
+Route::get('/register', Register::class)->name('register');
 
-Route::get('/employee/dashboard', [EmployeeController::class, 'dashboard'])->name('employee.dashboard');
-Route::get('/employee/add-user', [EmployeeController::class, 'view'])->name('employee.view-user');
-Route::post('/employee/add-user', [EmployeeController::class, 'store'])->name('employee.add-user');
+Route::get('/login', Login::class)->name('login');
+
+Route::get('/forgot-password', ForgotPassword::class)->name('forgot-password');
+
+Route::get('/reset-password/{id}', ResetPassword::class)->name('reset-password')->middleware('signed');
+
+Route::get('/404', Err404::class)->name('404');
+Route::get('/500', Err500::class)->name('500');
+Route::get('/upgrade-to-pro', UpgradeToPro::class)->name('upgrade-to-pro');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', Profile::class)->name('profile');
+    Route::get('/profile-example', ProfileExample::class)->name('profile-example');
+    Route::get('/users', Users::class)->name('users');
+    Route::get('/roles', Roles::class)->name('roles');
+    Route::get('/login-example', LoginExample::class)->name('login-example');
+    Route::get('/register-example', RegisterExample::class)->name('register-example');
+    Route::get('/forgot-password-example', ForgotPasswordExample::class)->name('forgot-password-example');
+    Route::get('/reset-password-example', ResetPasswordExample::class)->name('reset-password-example');
+    Route::get('/dashboard', Dashboard::class)->name('dashboard');
+    Route::get('/transactions', Transactions::class)->name('transactions');
+    Route::get('/bootstrap-tables', BootstrapTables::class)->name('bootstrap-tables');
+    Route::get('/lock', Lock::class)->name('lock');
+    Route::get('/buttons', Buttons::class)->name('buttons');
+    Route::get('/notifications', Notifications::class)->name('notifications');
+    Route::get('/forms', Forms::class)->name('forms');
+    Route::get('/modals', Modals::class)->name('modals');
+    Route::get('/typography', Typography::class)->name('typography');
+});
