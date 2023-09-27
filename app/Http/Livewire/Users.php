0f2b7e439933d20214ba77ffd5b81d40d4c5a0cc
XAPHNE <suvnkr.xaphene@gmail.com>
2023-09-27 21:19:24 +0530

    Update

--- app/Http/Livewire/Users.php
+++ app/Http/Livewire/Users.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Role;
+use App\Models\User;
 use Livewire\Component;
 
-class Users extends Component
-{
-    public function render()
-    {
+class Users extends Component {
+    public $users;
+    public $role;
+
+    public function render() {
+        $this->users = User::all();
+        $this->role = Role::with('roles')->find('id');
         return view('livewire.users');
     }
 }
