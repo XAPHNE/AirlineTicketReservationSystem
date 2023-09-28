a85619911ec2ded4cae59b7bf0ddcb0ecc2d5342
XAPHNE <suvnkr.xaphene@gmail.com>
2023-09-28 18:43:10 +0530

    Update

--- app/Http/Livewire/Roles.php
+++ app/Http/Livewire/Roles.php
@@ -15,6 +15,8 @@ class Roles extends Component
     public $confirmingRoleDeletion;
     public $sortField;
     public $sortDirection = 'asc'; // Default sorting direction
+    public $search = ''; // Add the search property
+    public $entries = 10; // Add the entries property
 
     protected $roleList = []; // Use a protected property
 
@@ -27,7 +29,7 @@ class Roles extends Component
     public function render()
     {
         return view('livewire.roles', [
-            'roles' => $this->getRoles(),
+            'roles' => $this->roleList, // Pass the roleList to the view
         ]);
     }
 
@@ -68,10 +70,23 @@ class Roles extends Component
         $this->roleList = $this->getRoles();
     }
 
+    public function updatedSearch()
+    {
+        // Update the roleList property with filtered data based on the search query
+        $this->roleList = $this->getRoles();
+    }
+
+    public function updatedEntries()
+    {
+        // Update the roleList property with the specified number of entries per page
+        $this->roleList = $this->getRoles();
+    }
+
     // Create a getter method for the roleList property
     protected function getRoles()
     {
-        return Role::orderBy($this->sortField, $this->sortDirection)->paginate(10);
+        return Role::where('name', 'like', '%' . $this->search . '%')
+            ->orderBy($this->sortField, $this->sortDirection)
+            ->paginate($this->entries);
     }
 }
-
